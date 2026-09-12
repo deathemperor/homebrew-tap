b0de2f559755b470c85a87f5734e5bfa8e8b9f83
infinitus-release <actions@github.com>
2026-09-12 16:02:35 +0000

    infinitus 0.5.0-alpha.4

--- Casks/infinitus.rb
+++ Casks/infinitus.rb
@@ -1,6 +1,6 @@
 cask "infinitus" do
-  version "0.5.0-alpha.3"
-  sha256 "5630f57ea496ba106da397803f935bcc6cb30c3f305af2870e6a3a24397fc3f9"
+  version "0.5.0-alpha.4"
+  sha256 "d4cdd23cf8497bc7131e4f6313f2f01cc169d02379487391d8bfa07bbdb4eaa9"
 
   url "https://github.com/deathemperor/infinitus/releases/download/v#{version}/Infinitus-#{version}.zip"
   name "Infinitus"
@@ -12,10 +12,10 @@ cask "infinitus" do
 
   app "Infinitus.app"
   binary "#{appdir}/Infinitus.app/Contents/MacOS/infinitusctl"
+  binary "#{appdir}/Infinitus.app/Contents/Resources/bin/infinitus"
   binary "#{appdir}/Infinitus.app/Contents/MacOS/ictl"
 
   caveats <<~EOS
-    The app drives the swapd engine:
-      cargo install --git https://github.com/deathemperor/swapd swapd
+    The swapd account engine is bundled. Open Infinitus Settings > Engines to get started.
   EOS
 end
